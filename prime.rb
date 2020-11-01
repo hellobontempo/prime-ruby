a4659ba7e3eec8cff8e70ea416f16951a4594c4e
claire <clairebontempo@gmail.com>
2020-11-01 08:15:25 -0800

    commit tree 343ca698c1d1b19a7085071ecf443bbb41e2469e

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+#define method #prime? 
+#return true prime numbers, false for non-prime
+# def prime? (num)
+#     return false if num.integer? == false
+#     return false if num < 2
+#     return true if num == 2
+#     (2..num-1).each {|n| return false if num % n == 0}
+#     true
+# end
+def prime? (num)
+    if num < 2
+        false
+    elsif num == 2
+        true
+    elsif (2..num-1).each {|n| return false if num % n == 0}
+    true
+end
+
+end
+
+
+    
